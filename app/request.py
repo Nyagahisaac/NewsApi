d017f98f3e504c728048816df5059ea62dfa62a2
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-09-13 22:47:34 +0300

    Define process_results method in our request.py to get news_list and transform into object

--- app/request.py
+++ app/request.py
@@ -50,6 +50,25 @@ def get_news():
             
     return news_results
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def process_results(news_list):
     '''
     Function  that processes the news result and transform them to a list of Objects
